306e9434e17290e919d18825db5cdbe097f0ec6a
MsekniFiras <124796124+MsekniFiras@users.noreply.github.com>
2024-07-21 13:05:14 +0100

    Add files via upload

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,24 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     private int score = 0;
     public int health = 5;
-
+    public Text scoreText;
+    public Text healthText;
+    public Rigidbody player;
+    public Text winlosetext;
+    public Image winlosebg;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    void Update()
-    {
-        if (health == 0)
-        {
-            Debug.Log("Game Over!");
-            SceneManager.LoadScene("maze");
-        }
-    }
-    // Update is called once per frame
     void FixedUpdate()
     {
         if (Input.GetKey("up"))
@@ -44,24 +39,54 @@ public class PlayerController : MonoBehaviour
         }
         
     }
-
     void OnTriggerEnter(Collider other) 
     {
         if (other.tag == "Pickup")
         {
-            score += 1;
+            score++;
+            SetScoreText();
             Destroy(other.gameObject);
-            Debug.Log("Score: " + score);
         }
         if (other.tag == "Trap")
         {
             health -= 1;
-            Debug.Log("Health: " + health);
+            SetHealthText();
         }
         if (other.tag == "Goal")
         {
-            Debug.Log("You win!");
+            winlosetext.text = "You Win!";
+            winlosetext.color = Color.black;
+            winlosebg.color = Color.green;
+            winlosebg.gameObject.SetActive(true);
+            StartCoroutine(LoadScene(3));
         }
     }
-
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+            winlosetext.text = "Game Over!";
+            winlosetext.color = Color.white;
+            winlosebg.color = Color.red;
+            winlosebg.gameObject.SetActive(true);
+            StartCoroutine(LoadScene(3));
+        }
+    }
+    
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + this.score.ToString();
+    }
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + this.health.ToString();
+    }
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene("Maze");
+        score = 0;
+        health = 5;
+    }
 }
